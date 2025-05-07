b0c408b8885206cd975644a24a9fe7863de16f16
Tito19201080 <198815280+Tito19201080@users.noreply.github.com>
2025-05-07 12:50:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// English Idioms
+
+document.getElementById("call").addEventListener("click",idiom1);
+function idiom1() {
+    alert("a day")
+}
+
+document.getElementById("cut").addEventListener("click",idiom2);
+function idiom2() {
+    alert("corners")
+}
+
+document.getElementById("some").addEventListener("click",idiom3);
+function idiom3() {
+    alert("slack")
+}
+
+document.getElementById("go").addEventListener("click",idiom4);
+function idiom4() {
+    alert("drawing board")
+}
+
+document.getElementById("hit").addEventListener("click",idiom5);
+function idiom5() {
+    alert("sack")
+}
